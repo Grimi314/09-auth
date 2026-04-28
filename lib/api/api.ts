ae00bd0de55422d2c3cb5c26dba0a7b5134a758a
Maryan Serhii <sergijmara210@gmail.com>
2026-04-28 18:22:20 +0300

    fix domain

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -3,7 +3,8 @@ const baseURL = process.env.NEXT_PUBLIC_API_URL
 export type ApiError = AxiosError<{ error: string }>
 
 export const api = axios.create({
+   withCredentials: true,
   baseURL: baseURL ,
-  withCredentials: true,
+ 
 });
 
